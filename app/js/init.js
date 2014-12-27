e5940e82cdf2f9f7e1449322f8ff2be0de3f8132
Afaque <ace.dne@gmail.com>
2014-12-27 21:50:31 +0300

    Completed top navigation bar with all styling and layout issues. Just need to update the html markup in topbar.jade as per our required pages.

--- app/js/init.js
+++ app/js/init.js
@@ -5,21 +5,15 @@ jQuery(document).ready(function($){
 		var scroll = $(window).scrollTop();
 
 		if (scroll >= 50) {
-			$('#logo-image').attr('src', 'img/logo-2.png')
 			$(".important-class").addClass("padding-on-my-header");
 		}
 		if (scroll < 50) {
 			$(".important-class").removeClass("padding-on-my-header");
-			$('#logo-image').attr('src', 'img/logo-1.png')
-		}
-	}).resize(function(){
-		if ( !smallWindow && this.innerWidth <= 1024 ) {
-			smallWindow = true;
-			$('.top-bar-section').find('ul.right').hide(0).delay(500).show(0);
-		}
-		if ( smallWindow && this.innerWidth > 1024 ) {
-			smallWindow = false;
 		}
 	});
+
+	$('.top-bar ul.right li').click(function() {
+       $('.top-bar').removeClass('expanded');
+    });
 });
 
