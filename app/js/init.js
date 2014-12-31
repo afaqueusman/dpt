a519d90268d6c0079a301554c70cdd5e753582d4
Afaque <ace.dne@gmail.com>
2014-12-31 19:27:09 +0300

    completed the secondary marketing section and added images

--- app/js/init.js
+++ app/js/init.js
@@ -1,13 +1,14 @@
 jQuery(document).ready(function($){
 
+	'use strict';
 	$(window).scroll(function() {
 		var scroll = $(window).scrollTop();
 
 		if (scroll >= 300) {
-			$(".important-class").addClass("padding-on-my-header");
+			$('.important-class').addClass('padding-on-my-header');
 		}
 		if (scroll < 300) {
-			$(".important-class").removeClass("padding-on-my-header");
+			$('.important-class').removeClass('padding-on-my-header');
 		}
 	});
 });
